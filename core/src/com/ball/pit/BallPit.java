c48f53c51055f0853538a336342dc2fae970a1c1
Billy Moroney <wjmoroney57@gmail.com>
2024-04-28 12:20:06 -0500

    next step of tutorial

--- core/src/com/ball/pit/BallPit.java
+++ core/src/com/ball/pit/BallPit.java
@@ -12,6 +12,7 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 
@@ -23,6 +24,10 @@ public class BallPit extends ApplicationAdapter {
 	public Array<ModelInstance> instances = new Array<ModelInstance>();
 	public boolean loading;
 	public Environment environment;
+	public Array<ModelInstance> invaders = new Array<ModelInstance>();
+	public Array<ModelInstance> blocks = new Array<ModelInstance>();
+	public ModelInstance ship;
+	public ModelInstance space;
 
 	// NEXT TIME pick pu from fbx-conv section of this:https://xoppa.github.io/blog/loading-models-using-libgdx/
 
@@ -36,7 +41,7 @@ public class BallPit extends ApplicationAdapter {
 		modelBatch = new ModelBatch();
 
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(7f, 7f, 7f);
+		cam.position.set(0f, 7f, 10f);
 		cam.lookAt(0,0,0);
 		cam.near = 1f;
 		cam.far = 300f;
@@ -45,30 +50,39 @@ public class BallPit extends ApplicationAdapter {
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
 
-//		ModelBuilder modelBuilder = new ModelBuilder();
-//		model = modelBuilder.createBox(5f, 5f, 5f,
-//				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-//				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-//		instance = new ModelInstance(model);
-
-//		ModelLoader loader = new ObjLoader();
-//		model = loader.loadModel(Gdx.files.internal("assets/ship.obj"));
-//		instance = new ModelInstance(model);
-
 		assets = new AssetManager();
-		assets.load("assets/ship.obj", Model.class);
+		assets.load("assets/ship.g3db", Model.class);
+		assets.load("assets/block.obj", Model.class);
+		assets.load("assets/invader.obj", Model.class);
+		assets.load("assets/spacesphere.obj", Model.class);
 		loading = true;
 	}
 
 	private void doneLoading() {
-		Model ship = assets.get("assets/ship.obj", Model.class);
+		ship = new ModelInstance(assets.get("assets/ship.g3db", Model.class));
+		ship.transform.setToRotation(Vector3.Y, 180).trn(0, 0, 6f);
+		instances.add(ship);
+
+		Model blockModel = assets.get("assets/block.obj", Model.class);
 		for (float x = -5f; x <= 5f; x += 2f) {
-			for (float z = -5f; z <= 5f; z += 2f) {
-				ModelInstance shipInstance = new ModelInstance(ship);
-				shipInstance.transform.setToTranslation(x, 0, z);
-				instances.add(shipInstance);
+			ModelInstance block = new ModelInstance(blockModel);
+			block.transform.setToTranslation(x, 0, 3f);
+			instances.add(block);
+			blocks.add(block);
+		}
+
+		Model invaderModel = assets.get("assets/invader.obj", Model.class);
+		for (float x = -5f; x <= 5f; x += 2f) {
+			for (float z = -8f; z <= 0f; z += 2f) {
+				ModelInstance invader = new ModelInstance(invaderModel);
+				invader.transform.setToTranslation(x, 0, z);
+				instances.add(invader);
+				invaders.add(invader);
 			}
 		}
+
+		space = new ModelInstance(assets.get("assets/spacesphere.obj", Model.class));
+
 		loading = false;
 	}
 
@@ -83,6 +97,8 @@ public class BallPit extends ApplicationAdapter {
 
 		modelBatch.begin(cam);
 		modelBatch.render(instances, environment);
+		if (space != null)
+			modelBatch.render(space);
 		modelBatch.end();
 	}
 	
@@ -91,5 +107,17 @@ public class BallPit extends ApplicationAdapter {
 		modelBatch.dispose();
 		instances.clear();
 		assets.dispose();
+	}
+
+	@Override
+	public void resume () {
+	}
+
+	@Override
+	public void resize (int width, int height) {
+	}
+
+	@Override
+	public void pause () {
 	}
 }
