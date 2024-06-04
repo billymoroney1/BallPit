368fa3a6a30cb934d51566f080aebf57d206b019
Billy Moroney <wjmoroney57@gmail.com>
2024-06-04 15:59:07 -0500

    transition to my own shit

--- core/src/com/ball/pit/BallPit.java
+++ core/src/com/ball/pit/BallPit.java
@@ -1,92 +1,64 @@
 package com.ball.pit;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
-import com.badlogic.gdx.graphics.g3d.model.Node;
-import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
-import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.g3d.utils.*;
+import com.badlogic.gdx.math.Intersector;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.math.collision.BoundingBox;
+import com.badlogic.gdx.math.collision.Ray;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class BallPit extends ApplicationAdapter {
-	public PerspectiveCamera cam;
-	public CameraInputController camController;
-	public ModelBatch modelBatch;
-	public AssetManager assets;
-	public Array<ModelInstance> instances = new Array<ModelInstance>();
-	public boolean loading;
-	public Environment environment;
-	public Array<ModelInstance> invaders = new Array<ModelInstance>();
-	public Array<ModelInstance> blocks = new Array<ModelInstance>();
-	public ModelInstance ship;
-	public ModelInstance space;
+public class BallPit extends InputAdapter implements ApplicationListener {
+	protected PerspectiveCamera cam;
+	protected CameraInputController camController;
+	protected ModelBatch modelBatch;
+	protected AssetManager assets;
+	protected Array<ModelInstance> instances = new Array<ModelInstance>();
+	protected Environment environment;
+	protected boolean loading;
 
-	// NEXT TIME pick pu from fbx-conv section of this:https://xoppa.github.io/blog/loading-models-using-libgdx/
-
-	
 	@Override
 	public void create () {
+		modelBatch = new ModelBatch();
 		environment = new Environment();
 		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
 		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-		modelBatch = new ModelBatch();
-
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set(0f, 7f, 10f);
+		cam.position.set(1f, 1f, 1f);
 		cam.lookAt(0,0,0);
 		cam.near = 1f;
-		cam.far = 300f;
+		cam.far = 600f;
 		cam.update();
 
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
 
 		assets = new AssetManager();
-		assets.load("assets/loadscene/data/invaderscene.g3db", Model.class);
+		assets.load("halfpipe.g3db", Model.class);
 		loading = true;
+
 	}
 
 	private void doneLoading() {
-		Model model = assets.get("assets/loadscene/data/invaderscene.g3db", Model.class);
-		for (int i = 0; i < model.nodes.size; i++){
-			String id = model.nodes.get(i).id;
-			ModelInstance instance = new ModelInstance(model, id);
-			Node node = instance.getNode(id);
-
-			instance.transform.set(node.globalTransform);
-			node.translation.set(0,0,0);
-			node.scale.set(1,1,1);
-			node.rotation.idt();
-			instance.calculateTransforms();
-
-			if (id.equals("space")) {
-				space = instance;
-				continue;
-			}
-
-			instances.add(instance);
-
-			if (id.equals("ship"))
-				ship = instance;
-			else if (id.startsWith("block"))
-				blocks.add(instance);
-			else if (id.startsWith("invader"))
-				invaders.add(instance);
-		}
-
-		loading = false;
+		Model halfPipe = assets.get("halfpipe.g3db", Model.class);
+		ModelInstance halfPipeInstance = new ModelInstance(halfPipe);
+		instances.add(halfPipeInstance);
 	}
 
+	private int visibleCount;
 	@Override
 	public void render () {
 		if (loading && assets.update())
@@ -98,11 +70,9 @@ public class BallPit extends ApplicationAdapter {
 
 		modelBatch.begin(cam);
 		modelBatch.render(instances, environment);
-		if (space != null)
-			modelBatch.render(space);
 		modelBatch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		modelBatch.dispose();
@@ -111,14 +81,14 @@ public class BallPit extends ApplicationAdapter {
 	}
 
 	@Override
-	public void resume () {
+	public void resize(int width, int height) {
 	}
 
 	@Override
-	public void resize (int width, int height) {
+	public void pause() {
 	}
 
 	@Override
-	public void pause () {
+	public void resume() {
 	}
 }
