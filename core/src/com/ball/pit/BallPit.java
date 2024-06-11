ef3888c6b8c5139978cafa469c7d1951a5e9c9dd
Billy Moroney <wjmoroney57@gmail.com>
2024-06-11 16:29:28 -0500

    current state

--- core/src/com/ball/pit/BallPit.java
+++ core/src/com/ball/pit/BallPit.java
@@ -3,21 +3,14 @@ package com.ball.pit;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.controllers.Controller;
+import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g3d.*;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.*;
-import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.math.Matrix4;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.math.collision.BoundingBox;
-import com.badlogic.gdx.math.collision.Ray;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.utils.Array;
 
 public class BallPit extends InputAdapter implements ApplicationListener {
@@ -28,6 +21,7 @@ public class BallPit extends InputAdapter implements ApplicationListener {
 	protected Array<ModelInstance> instances = new Array<ModelInstance>();
 	protected Environment environment;
 	protected boolean loading;
+	protected Controller controller;
 
 	@Override
 	public void create () {
@@ -42,6 +36,13 @@ public class BallPit extends InputAdapter implements ApplicationListener {
 		cam.near = 1f;
 		cam.far = 600f;
 		cam.update();
+
+		for (Controller controller : Controllers.getControllers()) {
+			Gdx.app.log("TAG", controller.getName());
+		}
+
+		controller = Controllers.getControllers().get(0);
+		System.out.println("axis count: " + controller.getAxisCount());
 
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
@@ -65,8 +66,15 @@ public class BallPit extends InputAdapter implements ApplicationListener {
 			doneLoading();
 		camController.update();
 
+		controller = Controllers.getCurrent();
+
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+		float leftStickX = controller.getAxis(0);
+		float leftStickY = controller.getAxis(1);
+		float rightStickX = controller.getAxis(2);
+		float rightStickY = controller.getAxis(3);
 
 		modelBatch.begin(cam);
 		modelBatch.render(instances, environment);
