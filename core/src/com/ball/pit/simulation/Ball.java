ec455c014a49f14fd22e492a30dc274555ba7175
Billy Moroney <wjmoroney57@gmail.com>
2024-07-11 08:58:25 -0500

    starting to get basic through rigid bodies tutorial

--- core/src/com/ball/pit/simulation/Ball.java
+++ core/src/com/ball/pit/simulation/Ball.java
@@ -3,11 +3,21 @@ package com.ball.pit.simulation;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.bullet.Bullet;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionObject;
 import com.badlogic.gdx.physics.bullet.collision.btCollisionShape;
+import com.badlogic.gdx.physics.bullet.collision.btSphereShape;
+import com.badlogic.gdx.physics.bullet.dynamics.btRigidBody;
 
 public class Ball extends ModelInstance {
+
+    public final btRigidBody body;
+    public boolean moving;
+    public final float mass = 1f;
+    public final btCollisionShape ballShape = new btSphereShape(1f);
+    public final btRigidBody.btRigidBodyConstructionInfo constructionInfo;
+    private static Vector3 localInertia = new Vector3();
     /*
     *
     * Set any important values related to ball size/velocity, lives etc.
@@ -18,10 +28,16 @@ public class Ball extends ModelInstance {
 
     public Ball (Model model) {
         super(model);
+        ballShape.calculateLocalInertia(mass, localInertia);
+        this.constructionInfo = new btRigidBody.btRigidBodyConstructionInfo(mass, null, ballShape, localInertia);
+        body = new btRigidBody(constructionInfo);
     }
 
     public Ball (Model model, float x, float y, float z) {
         super(model, x, y, z);
+        ballShape.calculateLocalInertia(mass, localInertia);
+        this.constructionInfo = new btRigidBody.btRigidBodyConstructionInfo(mass, null, ballShape, localInertia);
+        body = new btRigidBody(constructionInfo);
     }
 
     public void update (float delta) {
